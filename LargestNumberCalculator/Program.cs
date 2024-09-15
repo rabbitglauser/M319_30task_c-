ce188c8c3ae2201506c660f21921191789850a9a
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-09-15 22:15:01 +0200

    Add LargestNumberCalculator and PasswordValidator projects
    This commit introduces two new projects: LargestNumberCalculator and PasswordValidator. Both projects are added to the solution with their respective configurations and initial implementations. This setup includes all necessary project files and dependencies to ensure they compile and run correctly.

---
+++ LargestNumberCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the third number:");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+
+        int largestNumber = number1;
+
+        if (number2 > largestNumber)
+        {
+            largestNumber = number2;
+        }
+
+        if (number3 > largestNumber)
+        {
+            largestNumber = number3;
+        }
+
+        Console.WriteLine("The largest number is: " + largestNumber);
+    }
+}
---
+++ PasswordValidator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your password:");
+        string password = Console.ReadLine();
+
+        if (IsValidPassword(password))
+        {
+            Console.WriteLine("Password is valid.");
+        }
+        else
+        {
+            Console.WriteLine("Password does not meet the criteria.");
+        }
+    }
+
+    static bool IsValidPassword(string password)
+    {
+        if (password.Length < 8)
+        {
+            return false;
+        }
+
+        bool hasUpperCase = false;
+        bool hasLowerCase = false;
+        bool hasDigit = false;
+
+        foreach (char c in password)
+        {
+            if (char.IsUpper(c)) hasUpperCase = true;
+            if (char.IsLower(c)) hasLowerCase = true;
+            if (char.IsDigit(c)) hasDigit = true;
+        }
+
+        return hasUpperCase && hasLowerCase && hasDigit;
+    }
+}
