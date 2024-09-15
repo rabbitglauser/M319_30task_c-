36ec0a23d6cf5e83bdbed814b3a5b90a2ae1e740
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-09-15 18:50:25 +0200

    Add project files for SumOfTwoNumbers and OddAndEvenChecker
    This commit introduces new project files for both the SumOfTwoNumbers and OddAndEvenChecker applications, including necessary binaries and assembly info files. These additions set up the basic project structure and include all required dependencies and configuration files for initial build and execution.

---
+++ OddAndEvenChcker/OddEvenChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+                      
+namespace OddsAndEvens
+{
+    public class OddAndEvenChecker
+    {
+        public int UserInput { get; set; }
+
+        public void GetUserInput()
+        {
+            Console.WriteLine("Give me a number you want to check if it is even or odd:");
+            UserInput = int.Parse(Console.ReadLine());
+        }
+
+        public void OddAndEvenCalculator()
+        {
+            if (UserInput % 2 == 0)
+            {
+                Console.WriteLine("You entered an even number.");
+            }
+            else
+            {
+                Console.WriteLine("You entered an odd number.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            OddAndEvenChecker oddAndEvenChecker = new OddAndEvenChecker();
+            oddAndEvenChecker.GetUserInput();
+            oddAndEvenChecker.OddAndEvenCalculator();
+        }
+    }
+}
---
+++ SumOfTwoNumbers/SumOfTwoNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OddsAndEvens
+{
+    public class Calculator
+    {
+        public int UserInput1 { get; set; }
+        public int UserInput2 { get; set; }
+
+        public void GetUserInput()
+        {
+            Console.WriteLine("Enter the first number: ");
+            UserInput1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            UserInput2 = int.Parse(Console.ReadLine());
+        }
+
+        public int CalculateSum()
+        {
+            return UserInput1 + UserInput2;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Calculator calculator = new Calculator();
+            
+            calculator.GetUserInput();
+
+            Console.WriteLine($"The sum of the two entered numbers is {calculator.CalculateSum()}");
+        }
+    }
+}
