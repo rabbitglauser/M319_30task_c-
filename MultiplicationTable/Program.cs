27537e56b2452497d7348c8f347b8a3477d12cce
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-09-15 21:34:47 +0200

    Add MultiplicationTable project to solution
    Added MultiplicationTable.csproj and related files for building a .NET Core 3.1 console application. Included implementation of main program to generate a multiplication table based on user input. Updated solution file to include the new project.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask for user input
+            Console.WriteLine("Give me a number for the multiplication table:");
+            
+            // Parse user input into an integer
+            int UserInput = int.Parse(Console.ReadLine());
+
+            // Print multiplication table for the given number (from 1 to 10)
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{UserInput} x {i} = {UserInput * i}");
+            }
+        }
+    }
+}
