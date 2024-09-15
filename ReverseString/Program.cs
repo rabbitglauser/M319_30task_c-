cd9a9e00896837510baa8d5e7633a29de20d29c9
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-09-15 22:08:11 +0200

    Add ReverseString project.
    Introduced the ReverseString project into the solution, complete with implementation and necessary build files. This includes functionality to reverse user input strings and relevant project configuration files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in something so I can reverse it:");
+            string userInput  = Console.ReadLine();
+            
+            string reversedString = new string(userInput.Reverse().ToArray());
+            Console.WriteLine("Reversed String: " + reversedString);
+        }
+    }
+}
