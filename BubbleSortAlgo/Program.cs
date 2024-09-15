128fa78dbb34b218145a1cbf90f80e99890d0d3a
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-09-15 22:31:35 +0200

    Add BubbleSortAlgo and SmallestElementArray projects
    This commit introduces two new projects: BubbleSortAlgo and SmallestElementArray. BubbleSortAlgo contains an implementation of the bubble sort algorithm, while SmallestElementArray finds the smallest element in an array. Both projects are added to the solution file for seamless integration and build management.

---
+++ BubbleSortAlgo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of elements in the array:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        int[] array = new int[n];
+        
+        Console.WriteLine("Enter the elements of the array:");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        Console.WriteLine("Array before sorting:");
+        PrintArray(array);
+        
+        BubbleSort(array);
+
+        Console.WriteLine("Array after sorting:");
+        PrintArray(array);
+    }
+
+    static void BubbleSort(int[] array)
+    {
+        int n = array.Length;
+        bool swapped;
+        
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) 
+                break;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ SmallestElementArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of elements in the array:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        int[] array = new int[n];
+        
+        Console.WriteLine("Enter the elements of the array:");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        int smallestElement = FindSmallestElement(array);
+
+        Console.WriteLine($"The smallest element in the array is: {smallestElement}");
+    }
+
+    static int FindSmallestElement(int[] array)
+    {
+        int smallest = array[0];
+        
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < smallest)
+            {
+                smallest = array[i];
+            }
+        }
+        
+        return smallest;
+    }
+}
